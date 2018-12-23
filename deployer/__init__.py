c328a6a3d6ce39896feccdb7487972659addf444
Gareth Brown <gaz@gbmail.me>
2018-12-23 13:18:19 +0000

    initial commit for checker module

--- deployer/__init__.py
+++ deployer/__init__.py
@@ -3,7 +3,6 @@ from __future__ import print_function, absolute_import
 
 import json
 import python_terraform
-import terraform_validate
 import os
 import builder
 
@@ -32,7 +31,7 @@ def add_args():
 
 def main():
     settings = add_args()
-    # needs to differentiate between orgs and repo for code and config 
+    # needs to differentiate between orgs and repo for code and config
     config_org = builder.get_org(settings)
     config_repo = builder.get_repo(config_org, settings.project_name)
     config_files = builder.get_files(settings.config_version, config_repo)
@@ -41,12 +40,12 @@ def main():
     code_org = builder.get_org(settings)
     code_repo = builder.get_repo(code_org, settings.project_name)
     code_files = builder.get_files(settings.code_version, code_repo)
-    # pass through code checker 
-    # test deploy 
-    # compare test project state file against actual state file 
+    # pass through code checker
+    # test deploy
+    # compare test project state file against actual state file
     # plan
-    # deploy 
-    # validate 
+    # deploy
+    # validate
     pass
 
 
