e1d167f0df899dbed741f396a196aecccdb704c5
demid_avramenko <demid.avramenko@mesoform.com>
2019-11-22 16:25:27 +0200

    Rework stackdriver reporter to use new basic class

--- reporter/stackdriver.py
+++ reporter/stackdriver.py
@@ -2,25 +2,25 @@ from time import sleep
 
 from datetime import datetime
 
-from google.api.metric_pb2 import MetricDescriptor
-from google.cloud.monitoring_v3 import MetricServiceClient
-from google.auth.credentials import Credentials
+from time import sleep
 
+from google.api.metric_pb2 import MetricDescriptor
+from google.auth.credentials import Credentials
+from google.cloud.monitoring_v3 import MetricServiceClient
 from google.cloud.monitoring_v3.types import TimeSeries
 
-
-class SerializationException(Exception):
-    pass
+from .base import MetricsRegistry, MetricsReporter
 
 
-class StackdriverModuleException(Exception):
-    pass
-
-
-class MessageSerializer:
+class StackdriverReporterException(Exception):
     """
-    Performs serialization of raw metrics data to constructed message, that
-    can be taken and processed by stackdriver reporting backend.
+    Represents errors for stackdriver reporter
+    """
+
+
+class StackdriverReporter(MetricsReporter):
+    """
+    Implementation of metrics reporter, that sends metrics to Google Stackdriver.
     """
 
     metric_kinds = {
@@ -38,160 +38,108 @@ class MessageSerializer:
 
     units = ("s", "min", "h", "d")
 
-    def deserialize(self, raw_data):
-        self._validate_raw_data(raw_data)
-
-        deserialized = raw_data.copy()
-
-        deserialized["metric_kind"] = self.metric_kinds[
-            deserialized["metric_kind"]
-        ]
-        deserialized["value_type"] = self.value_types[
-            deserialized["value_type"]
-        ]
-
-        return deserialized
-
-    def _validate_raw_data(self, raw_data):
-        if not isinstance(raw_data, dict):
-            raise SerializationException(
-                f"Wrong data, should be dict: {raw_data}"
-            )
-
-        if raw_data["metric_kind"] not in self.metric_kinds:
-            raise SerializationException(
-                f"Wrong metric kind: {raw_data['metric_kind']},"
-                f"should be one of {list(self.metric_kinds.keys())}"
-            )
-
-        if raw_data["value_type"] not in self.value_types:
-            raise SerializationException(
-                f"Wrong value type: {raw_data['value_type']},"
-                f"should be one of {list(self.value_types.keys())}"
-            )
-
-        if "unit" in raw_data and raw_data["unit"] not in self.units:
-            raise SerializationException(
-                f"Wrong unit: {raw_data['unit']},"
-                f"should be one of {self.units}"
-            )
-
-    def _is_value_valid(self, value):
-        """
-        Here should be some validation of raw metric data value.
-        """
-
-
-class Metrics(object):
     def __init__(
-        self,
-        monitoring_project: str,
-        monitoring_credentials: Credentials,
-        metrics_set_list: list = None,
-        metrics_client=MetricServiceClient,
-        metrics_type=TimeSeries,
-        complete_message=None,
-        serializer_class=MessageSerializer,
+        self, monitoring_project: str, monitoring_credentials: Credentials
     ):
-        self._monitoring_project: str = monitoring_project
-        self._monitoring_credentials: Credentials = monitoring_credentials
-        self._metrics_set_list: list = metrics_set_list or []
-        self._metrics_client = metrics_client
-        self._metrics_type = metrics_type
-        self._complete_message = complete_message
-        self._serializer = serializer_class()
+        super().__init__()
 
-    @property
-    def complete_message(self):
-        """
-        Completely constructed and initialized Protobuf message for given metrics_type
-        """
-        return self._complete_message
+        self.monitoring_project: str = monitoring_project
+        self.monitoring_credentials: Credentials = monitoring_credentials
+        self.metrics_client = MetricServiceClient(
+            credentials=self.monitoring_credentials
+        )
+        self.metrics_type = TimeSeries
 
-    @complete_message.setter
-    def complete_message(self, value):
-        self._complete_message = value
-
-    @property
-    def metrics_type(self):
-        return self._metrics_type
-
-    @metrics_type.setter
-    def metrics_type(self, class_):
-        self._metrics_type = class_
-
-    @property
-    def metrics_client(self):
-        return self._metrics_client(credentials=self.monitoring_credentials)
-
-    @metrics_client.setter
-    def metrics_client(self, class_):
-        self._metrics_client = class_
-
-    @property
-    def monitoring_credentials(self):
-        return self._monitoring_credentials
-
-    @monitoring_credentials.setter
-    def monitoring_credentials(self, value: str):
-        self._monitoring_credentials = value
-
-    @property
-    def monitoring_project(self):
-        return self._monitoring_project
-
-    @monitoring_project.setter
-    def monitoring_project(self, value: str):
-        self._monitoring_project = value
+        self.start_time = datetime.utcnow()
+        self.end_time = None
 
     @property
     def monitoring_project_path(self):
         return self.metrics_client.project_path(self.monitoring_project)
 
-    @property
-    def metrics_set_list(self):
+    def validate_metric_registry(self, metric_registry: MetricsRegistry):
         """
-        metrics_set_list is a list of tuples in the form of:
-        (matric_name, metric_labels, value_type, metric_value)
-        value type can be: int64, bool, double, string, distribution
+        Ensures, that metrics registry contain all required data.
+        """
+        raw_record = metric_registry.raw_record
 
-        For example:
-        ("runtime", {"type": "seconds"}, "int", 5})
-        :return: list
-        """
-        return self._metrics_set_list
+        if not isinstance(raw_record, dict):
+            raise StackdriverReporterException(
+                f"Wrong data, should be dict: {raw_record}"
+            )
 
-    @metrics_set_list.setter
-    def metrics_set_list(self, metrics_sets: list):
+        for key in (
+            "metric_name",
+            "labels",
+            "metric_kind",
+            "value_type",
+            "value",
+        ):
+            if key not in raw_record:
+                raise StackdriverReporterException(
+                    f'Key "{key}" is required for stackdriver metric registry.'
+                )
+
+        if raw_record["metric_kind"] not in self.metric_kinds:
+            raise StackdriverReporterException(
+                f"Wrong metric kind: {raw_record['metric_kind']},"
+                f"should be one of {list(self.metric_kinds.keys())}"
+            )
+
+        if raw_record["value_type"] not in self.value_types:
+            raise StackdriverReporterException(
+                f"Wrong value type: {raw_record['value_type']},"
+                f"should be one of {list(self.value_types.keys())}"
+            )
+
+        if "unit" in raw_record and raw_record["unit"] not in self.units:
+            raise StackdriverReporterException(
+                f"Wrong unit: {raw_record['unit']},"
+                f"should be one of {self.units}"
+            )
+
+    def prepare_metric_registry(self, metric_registry: MetricsRegistry):
         """
-        takes a list of tuples in the format described above
-        :param value: list of dicts
+        Fulfills `prepared_record` of metric registry with implementation-specific
+        metrics data, like protobuf descriptors.
         """
-        self._metrics_set_list = [
-            self._serializer.deserialize(metric) for metric in metrics_sets
+        prepared_record = metric_registry.raw_record.copy()
+
+        prepared_record["metric_kind"] = self.metric_kinds[
+            prepared_record["metric_kind"]
+        ]
+        prepared_record["value_type"] = self.value_types[
+            prepared_record["value_type"]
         ]
 
-    def add_metric_set(self, metrics_set):
-        metric_set = self._serializer.deserialize(metrics_set)
-        self._metrics_set_list.append(metric_set)
+        metric_registry.prepared_record = prepared_record
 
-    def initialize_base_metrics_message(self, metric_name, labels):
-        pass
+    def _create_metric_descriptor(
+        self, metric_kind, value_type, metric_name, unit
+    ):
+        """
+        Creates metric descriptor.
+        We need this because `TimeSeries` protobuf message doesn't allow to specify units, so we need
+        to create metric descriptor with separate request.
+        """
+        metric_descriptor_values = {
+            "metric_kind": metric_kind,
+            "value_type": value_type,
+            "type": f"custom.googleapis.com/{metric_name}",
+        }
+        if unit is not None:
+            metric_descriptor_values["unit"] = unit
 
-    def add_data_points_to_metric_message(self, message, data_points):
-        pass
+        self.metrics_client.create_metric_descriptor(
+            name=self.monitoring_project_path,
+            metric_descriptor=MetricDescriptor(**metric_descriptor_values),
+        )
 
-    def send_metrics(self):
-        pass
+        # is we send requests through metrics_client one after another, we are receiving unclear error 500,
+        # probably due to google's requests throttling
+        sleep(1)
 
-
-class AppMetrics(Metrics):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self._start_time = datetime.utcnow()
-        self._end_time = None
-
-    def initialize_base_metrics_message(
+    def _initialize_base_metrics_message(
         self,
         metric_name: str,
         labels: dict,
@@ -208,17 +156,8 @@ class AppMetrics(Metrics):
         :param unit: The unit in which the metric value is reported.
         :return: ::google.cloud.monitoring_v3.types.TimeSeries::
         """
-        metric_descriptor_values = {
-            "metric_kind": metric_kind,
-            "value_type": value_type,
-            "type": f"custom.googleapis.com/{metric_name}",
-        }
-        if unit is not None:
-            metric_descriptor_values["unit"] = unit
-
-        self.metrics_client.create_metric_descriptor(
-            name=self.monitoring_project_path,
-            metric_descriptor=MetricDescriptor(**metric_descriptor_values),
+        self._create_metric_descriptor(
+            metric_kind, value_type, metric_name, unit
         )
 
         # if we send requests through metrics_client one after another, we receive unclear error 500,
@@ -234,9 +173,9 @@ class AppMetrics(Metrics):
         series.metric.labels.update(labels)
         return series
 
-    def add_data_points_to_metric_message(self, message: TimeSeries, value):
+    def _add_data_points_to_metric_message(self, message: TimeSeries, value):
         """
-        takes an initialized TimeSeries Protobuf message object and adds data_point_value with the
+        Takes an initialized TimeSeries Protobuf message object and adds data_point_value with the
         end_time as now()
         :param message: TimeSeries object
         :param value: value to add to data point
@@ -249,15 +188,15 @@ class AppMetrics(Metrics):
         }
         attribute = message_value_attributes.get(message.value_type)
         if not attribute:
-            raise StackdriverModuleException(
+            raise StackdriverReporterException(
                 f"Unexpected value type: {message.value_type}"
             )
 
         data_point = message.points.add()
         setattr(data_point.value, attribute, value)
 
-        if self._start_time and message.metric_kind != MetricDescriptor.GAUGE:
-            data_point.interval.start_time.FromDatetime(self._start_time)
+        if self.start_time and message.metric_kind != MetricDescriptor.GAUGE:
+            data_point.interval.start_time.FromDatetime(self.start_time)
 
         end_time = self.end_time if self.end_time else datetime.utcnow()
 
@@ -265,13 +204,18 @@ class AppMetrics(Metrics):
         return message
 
     def send_metrics(self):
+        """
+        Constructs protobuf messages and sends them through client.
+        """
         time_series_list = []
 
-        for metrics_set in self.metrics_set_list:
-            value = metrics_set.pop("value")
+        for metrics_registry in self.metrics_registry_set:
+            value = metrics_registry.prepared_record.pop("value")
 
-            base_metrics = self.initialize_base_metrics_message(**metrics_set)
-            time_series = self.add_data_points_to_metric_message(
+            base_metrics = self._initialize_base_metrics_message(
+                **metrics_registry.prepared_record
+            )
+            time_series = self._add_data_points_to_metric_message(
                 base_metrics, value
             )
 
@@ -282,13 +226,5 @@ class AppMetrics(Metrics):
         )
 
     @property
-    def end_time(self):
-        return self._end_time
-
-    @end_time.setter
-    def end_time(self, value):
-        self._end_time = value
-
-    @property
     def app_runtime(self):
-        return self._end_time - self._start_time
+        return self.end_time - self.start_time
