f642f663280963c3038818b22a88f38da72f7ab5
demid_avramenko <demid.avramenko@mesoform.com>
2019-11-22 16:23:43 +0200

    Add basic reporter and metrics classes

---
+++ reporter/base.py
@@ -0,0 +1,40 @@
+class MetricsRegistry:
+    """
+    Instances of this class contain original and prepared metrics data
+    """
+
+    def __init__(self, record):
+        if not isinstance(record, dict):
+            raise ValueError(
+                "Record passed to MetricRegistry should be type of dict."
+            )
+
+        self._raw_record = record
+        self.prepared_record = {}
+
+    @property
+    def raw_record(self):
+        return self._raw_record
+
+
+class MetricsReporter:
+    """
+    Base class for reporting backends
+    """
+
+    def __init__(self, *args, **kwargs):
+        self.metrics_registry_set = []
+
+    def add_metric_registry(self, metric_registry: MetricsRegistry):
+        self.validate_metric_registry(metric_registry)
+        self.prepare_metric_registry(metric_registry)
+        self.metrics_registry_set.append(metric_registry)
+
+    def send_metrics(self):
+        raise NotImplementedError
+
+    def prepare_metric_registry(self, metric_registry: MetricsRegistry):
+        raise NotImplementedError
+
+    def validate_metric_registry(self, metric_registry: MetricsRegistry):
+        raise NotImplementedError
