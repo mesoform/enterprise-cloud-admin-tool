52a486139746f16d6cd5dacd57db4d33b0f06e4a
danperezsan <dan@mesoform.com>
2023-04-25 17:47:14 +0200

    Revert "TeamCity change in 'Enterprise Cloud Admin' project: build features of 'Unit Testing' build configuration were updated"
    This reverts commit 8acf06a4c1e4e930a2ec6d9ff5e1d7cdf5ac0728.

--- .teamcity/patches/buildTypes/Build.kts
+++ .teamcity/patches/buildTypes/Build.kts
@@ -1,8 +1,6 @@
 package patches.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2018_2.*
-import jetbrains.buildServer.configs.kotlin.v2018_2.failureConditions.BuildFailureOnMetric
-import jetbrains.buildServer.configs.kotlin.v2018_2.failureConditions.failOnMetricChange
 import jetbrains.buildServer.configs.kotlin.v2018_2.ui.*
 
 /*
@@ -22,15 +20,5 @@ changeBuildType(RelativeId("Build")) {
             "Unexpected option value: executionTimeoutMin = $executionTimeoutMin"
         }
         executionTimeoutMin = 15
-        add {
-            failOnMetricChange {
-                metric = BuildFailureOnMetric.MetricType.BUILD_DURATION
-                threshold = 3600
-                units = BuildFailureOnMetric.MetricUnit.DEFAULT_UNIT
-                comparison = BuildFailureOnMetric.MetricComparison.MORE
-                compareTo = value()
-                param("anchorBuild", "lastSuccessful")
-            }
-        }
     }
 }
